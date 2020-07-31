8bfedad388684015ec36f0462fe4a22477235fa1
GenildoLopes <carvalhogenildo800@gmail.com>
2020-07-31 11:18:00 -0300

    Index test

--- main.js
+++ main.js
@@ -1,23 +1,3 @@
-const electron = require('electron')
-
-const { app, BrowserWindow } = require('electron')
-
-function createWindow () {
-  // Cria uma janela de navegação.
-  let win = new BrowserWindow({
-    width: 800,
-    height: 600,
-    webPreferences: {
-      nodeIntegration: true
-    }
-  })
-
-  // e carregar o index.html do aplicativo.
-  win.loadFile('index.html')
-}
-
-app.whenReady().then(createWindow)
-
 const { app, BrowserWindow } = require('electron')
 
 function createWindow () {
